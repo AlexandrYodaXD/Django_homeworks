6a43e2c489e1d5b07911e2c5ab3d3c89689f00fd
YodaXD <LyZNF@B@9f42E64>
2024-02-18 17:00:35 +0300

    hw_2 сделаны большие правки в моделях и методах работы с заказами

--- django_project/hw_2/management/commands/create_order.py
+++ django_project/hw_2/management/commands/create_order.py
@@ -1,18 +1,49 @@
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.management.base import BaseCommand
+from django.db import transaction
 # noinspection PyUnresolvedReferences
-from hw_2.models import Order
+from hw_2.models import Order, Product
 
 
 class Command(BaseCommand):
+    help = 'Create an order with specified products'
+
     def add_arguments(self, parser):
-        parser.add_argument('client_id', type=int)
-        parser.add_argument('product_id', type=int)
-        parser.add_argument('total_price', type=float)
+        parser.add_argument('client_id', type=int, help='ID of the client')
+        parser.add_argument('products_data', nargs='+', type=str,
+                            help='List of products with quantities (format: "product_id x quantity")')
 
     def handle(self, *args, **kwargs):
         client_id = kwargs['client_id']
-        product_id = kwargs['product_id']
-        total_price = kwargs['total_price']
-        order = Order.objects.create(client_id=client_id, product_id=product_id, total_price=total_price)
-        order.save()
-        self.stdout.write(self.style.SUCCESS(f'Order {order.id} created successfully'))
+        products_data = kwargs['products_data']
+
+        # Создаем словарь для хранения пар идентификатора товара и его количества
+        product_quantities = dict()
+
+        # Заполняем словарь пар идентификатора продукта и его количества
+        for product in products_data:
+            try:
+                product_id, quantity = map(int, product.split('x'))
+            except ValueError:
+                self.stdout.write(self.style.ERROR('Invalid product data'))
+                return
+            try:
+                product = Product.objects.get(id=product_id)
+                product_quantities[product] = product_quantities.get(product, 0) + quantity
+            except ObjectDoesNotExist:
+                self.stdout.write(self.style.ERROR(f'Product with ID {product_id} does not exist'))
+                return
+
+        with transaction.atomic():
+            try:
+                # Создаем заказ
+                order = Order(client_id=client_id)
+                order.create_order(product_quantities)
+
+                # Добавляем продукты в заказ
+            except ValidationError as e:
+                self.stdout.write(self.style.ERROR(e.message))
+                return
+
+            total_price = order.total_price
+            self.stdout.write(self.style.SUCCESS(f'Order {order.id} created successfully with total price {total_price}'))
--- django_project/hw_2/management/commands/get_order_by_id.py
+++ django_project/hw_2/management/commands/get_order_by_id.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 # noinspection PyUnresolvedReferences
 from hw_2.models import Order
@@ -9,5 +10,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         order_id = kwargs['order_id']
-        order = Order.objects.get(id=order_id)
-        self.stdout.write(self.style.SUCCESS(f'Order {order.id} found successfully'))
+        try:
+            order = Order.objects.get(id=order_id)
+            self.stdout.write(self.style.SUCCESS(f'Order {order.id} found successfully'))
+            self.stdout.write(f'Created at: {order.created_at}')
+            self.stdout.write(f'Client:'
+                              f'\n-id: {order.client.id}'
+                              f'\n-name: {order.client.name}')
+            self.stdout.write('Products:')
+            for item in order.orderitem_set.all():
+                self.stdout.write(f'- {item.product.name} ({item.product.price}$): {item.quantity} PC.')
+            self.stdout.write(f'Total price: {order.total_price}$')
+        except ObjectDoesNotExist:
+            self.stdout.write(self.style.ERROR(f'Order with ID {order_id} does not exist'))
+            return
+
